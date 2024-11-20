b65e6bc65cccf2fa954be16070c2d9dff0ea45e0
JonasDesodt <95071893+JonasDesodt@users.noreply.github.com>
2024-11-20 19:59:19 +0100

    api request

--- controllers/home_controller.php
+++ controllers/home_controller.php
@@ -4,6 +4,22 @@
         //GET
         public function index()
         {
+            $apiKey = 'NQGWMiK9/kGzUp9Qf9TagM2d0uyUEPT6RY5Qx0Eq0zY=';
+            $url = 'https://localhost:7023/public/Agenda?Page=1&PageSize=12';
+
+            $ch = curl_init($url);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // Disable SSL verification, dev only!
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false); // Disable hostname verification, dev only!
+            
+            $response = curl_exec($ch);
+            curl_close($ch);
+            
+            echo $response;
+
+
+
+
             $view_path = 'views/home/index.php';
 
             $data = 'Data from home controller';
--- main.php
+++ main.php
@@ -3,6 +3,8 @@
     {
         public function run($layout_path)    
         {
+            // https://www.php.net/manual/en/book.curl.php
+
             require('router.php');
 
             $context = (new Router)->delegate($_SERVER['REQUEST_URI']);           
