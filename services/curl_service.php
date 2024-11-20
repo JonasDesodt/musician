c69c8cc5643165970f28a8bf675e023454220c6b
JonasDesodt <95071893+JonasDesodt@users.noreply.github.com>
2024-11-20 20:26:26 +0100

    get w/ api-key

--- services/curl_service.php
+++ services/curl_service.php
@@ -10,7 +10,17 @@
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // Disable SSL verification, dev only!
             curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false); // Disable hostname verification, dev only!
-            
+            curl_setopt($ch, CURLOPT_HTTPHEADER, [
+                'Accept-Language: en',
+                'Application-Name: Laravel',
+                'Api-Key: ' . $apiKey // Or other header format like 'x-api-key'
+            ]);
+
+            // 'Accept-Language' => app()->getLocale(),
+            // 'Application-Name' => 'Laravel',
+            // 'Api-Key' => 'NQGWMiK9/kGzUp9Qf9TagM2d0uyUEPT6RY5Qx0Eq0zY='
+
+
             $response = curl_exec($ch);
             curl_close($ch);
             
